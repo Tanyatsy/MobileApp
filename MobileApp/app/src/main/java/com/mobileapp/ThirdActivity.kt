dbaa025728003c8193548f18347b18ae5b6e60de
tanya <tatiana.t@xor.ai>
2021-03-10 23:51:42 +0200

    Add list with two columns

--- MobileApp/app/src/main/java/com/mobileapp/ThirdActivity.kt
+++ MobileApp/app/src/main/java/com/mobileapp/ThirdActivity.kt
@@ -1,6 +1,5 @@
 package com.mobileapp
 
-import android.R
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView.OnItemClickListener
@@ -9,36 +8,37 @@ import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.mobileapp.R.layout.activity_third)
 
-        val listView: ListView = findViewById<ListView>(com.mobileapp.R.id.listView)
-        // Defined Array values to show in ListView
-        val values = arrayOf(
-            "Android List View",
-            "Adapter implementation",
-            "Simple List View In Android",
-            "Create List View Android",
-            "Android Example",
-            "List View Source Code",
-            "List View Array Adapter",
-            "Android Example List View"
+        val values: Map<String,String> = mapOf<String,String>(
+            "entity" to "организация", "work out" to "решить", "congestion" to "затор",
+            "vendor" to "продавец", "threat" to "угроза", "engage" to "обручаться",
+            "ban" to "запрет", "appreciate" to "ценить", "offend" to "обижать",
+            "guilt" to "вина", "hurt" to "ранить", "merry" to "радостный",
+            "scared" to "напуганный", "angry" to "злой", "unhappy" to "нестастный",
+            "optimism" to "оптимизм", "desire" to "желание", "fear" to "страх",
+            "entity" to "организация", "work out" to "решить", "congestion" to "затор",
+            "terrible" to "ужасный", "work out" to "решить", "congestion" to "затор",
+            "" to "организация", "work out" to "решить", "congestion" to "затор",
+            "entity" to "организация", "work out" to "решить", "congestion" to "затор",
+            "entity" to "организация", "work out" to "решить", "congestion" to "затор"
         )
+        linearLayoutManager = LinearLayoutManager(this)
 
-        val adapter = ArrayAdapter(
-            this,
-            R.layout.simple_list_item_1, R.id.text1, values
-        )
+        val adapter = ListAdapter(values)
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = linearLayoutManager
 
-        listView.adapter = adapter
-
-        listView.onItemClickListener = OnItemClickListener { _, _, position, _ -> // TODO Auto-generated method stub
-            val value = adapter.getItem(position)
-            Toast.makeText(applicationContext, value, Toast.LENGTH_SHORT).show()
-        }
     }
 }
